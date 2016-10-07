6392ee5052ec9d64aa0971b1143e1fd8198d5f12
shiehyaotsung <shiehyaotsung@gmail.com>
2016-10-07 15:11:08 +0800

    Create RaspberryPi3-AP.sh

---
+++ RaspberryPi3-AP.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+# ==========================================================================
+if [ "$EUID" -ne 0 ]
+    then [ "$LC_TIME" = "zh_TW.UTF-8" ]&& echo "必須是 root, 會自動使用sudo"||echo "Must be root, so use sudo automatically."
+fi
+# ==========================================================================
+if [[ $# -lt 2 ]]; then 
+    if [ "$LC_TIME" = "zh_TW.UTF-8" ]; then 
+        echo "請您設定WiFi名稱及密碼（至少8碼）作為參數"
+        echo "格式如下:"
+        echo "sudo  $0   WiFi名稱   密碼"
+        echo "sudo  $0   R-Pi3   raspberry"
+        echo "sudo  $0   WiFi名稱   密碼  區域IP前3碼  區域IP最後一碼開頭  區域IP最後一碼結尾"
+        echo "sudo  $0   R-Pi3   raspberry  172.18.1  100  160"
+    else
+        echo "Please use WiFiName and password（minimum: 8 characters）"
+        echo "Usage:"
+        echo "sudo  $0   WiFiName   password"
+        echo "sudo  $0   R-Pi3   raspberry"
+        echo "sudo  $0   WiFiName   password  localIP  from  to"
+        echo "sudo  $0   R-Pi3   raspberry  172.18.1  100  160"
+    fi
+    exit
+fi
+# ==========================================================================
+wifi_ID="$1"
+wifiPassword="$2"
+LocalIP=${3:-'172.18.1'}
+LocalIPfrom=${4:-100}
+LocalIPto=${5:-160}
+# ==========================================================================
+sudo apt-get install dnsmasq hostapd
+# ==========================================================================
+grep 'denyinterfaces wlan0' /etc/dhcpcd.conf  &> /dev/null
+(($?==0))  || echo 'denyinterfaces wlan0' >> /etc/dhcpcd.conf
+# ==========================================================================
+grep 'allow-hotplug wlan0' /etc/network/interfaces &> /dev/null
+if [[ $? -eq 0 ]]; then
+   nn=$(grep -n 'iface wlan0 inet static' /etc/network/interfaces|cut -d : -f 1)
+   if ((nn>0)); then 
+        mm=$((nn+4))
+        while (( mm>nn ))
+        do 
+            sed -n "${mm}p" /etc/network/interfaces| egrep "^[[:space:]]*(address|netmask|network|broadcast)"  > /dev/null
+            (($?==0))  && sudo sed -i "${mm}d" /etc/network/interfaces
+            ((--mm))
+        done
+        sudo sed -i "${nn}aaddress ${LocalIP}.1\nnetmask 255.255.255.0\nnetwork ${LocalIP}.0\nbroadcast ${LocalIP}.255" /etc/network/interfaces
+   else
+        sudo sed -i "s/iface wlan0 inet manual/iface wlan0 inet static\naddress ${LocalIP}.1\nnetmask 255.255.255.0\nnetwork ${LocalIP}.0\nbroadcast ${LocalIP}.255/" /etc/network/interfaces
+   fi
+   egrep "^[[:space:]]*wpa-conf \/etc\/wpa_supplicant\/wpa_supplicant.conf" /etc/network/interfaces > /dev/null && \
+        sudo sed -i 's/^[[:space:]]*wpa-conf \/etc\/wpa_supplicant\/wpa_supplicant.conf/# wpa-conf \/etc\/wpa_supplicant\/wpa_supplicant.conf/' /etc/network/interfaces
+fi
+# ==========================================================================
+sudo service dhcpcd restart
+sudo ifdown wlan0; sudo ifup wlan0
+# ==========================================================================
+sudo sed -i 's/#DAEMON_CONF=""/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/'  /etc/default/hostapd
+# ==========================================================================
+sudo bash -c "cat > /etc/hostapd/hostapd.conf" <<EOF
+interface=wlan0
+driver=nl80211
+hw_mode=g
+channel=6
+ieee80211n=1
+wmm_enabled=1
+ht_capab=[HT40][SHORT-GI-20][DSSS_CCK-40]
+macaddr_acl=0
+auth_algs=1
+ignore_broadcast_ssid=0
+ssid=$wifi_ID
+wpa_passphrase=$wifiPassword
+wpa=2
+wpa_key_mgmt=WPA-PSK
+rsn_pairwise=CCMP
+EOF
+# ==========================================================================
+sudo bash -c "cat > /etc/dnsmasq.conf" <<EOF
+interface=wlan0
+listen-address=${LocalIP}.1
+bind-interfaces
+server=8.8.8.8
+domain-needed
+bogus-priv
+dhcp-range=${LocalIP}.${LocalIPfrom},${LocalIP}.${LocalIPto},12h
+EOF
+# ==========================================================================
+sudo sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
+sudo bash -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
+# ==========================================================================
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE  
+sudo iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT  
+sudo iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT  
+sudo bash -c "iptables-save > /etc/iptables.ipv4.nat"
+sudo sed -i 's/exit 0/iptables-restore \< \/etc\/iptables.ipv4.nat\nexit 0/g' /etc/rc.local
+# ==========================================================================
+sudo service hostapd restart  
+sudo service dnsmasq restart  
+# ==========================================================================
+ps aux | grep hostapd | grep -v grep  | grep hostapd
+ps aux | grep dnsmasq | grep -v grep | grep dnsmasq
